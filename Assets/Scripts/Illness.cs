4097ee95f43e1f8f793b039ce98c57d58d94d86b
DavidLindsayV <83639079+DavidLindsayV@users.noreply.github.com>
2022-08-26 12:09:44 +1200

    updated illness script

--- Assets/Scripts/Illness.cs
+++ Assets/Scripts/Illness.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Illness : MonoBehaviour
 {
+    public GameObject player;
+    public int sightLength = 8;
+    public int enemySpeed;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        this.rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(this.gameObject.transform.position, player.transform.position) < sightLength){
+            Vector3 dir = (player.transform.position - this.gameObject.transform.position).normalized;
+            rb.velocity = new Vector2(enemySpeed, dir);
+        }
     }
 }
